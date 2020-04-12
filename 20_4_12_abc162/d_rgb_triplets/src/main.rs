123c983ece92bb24462c3347f7a1cf3eda82aab0
UMASHIBA <umashiba1@gmail.com>
2020-04-12 23:49:41 +0900

    abc162のdの時間制限超過を起こした解法1

--- 20_4_12_abc162/d_rgb_triplets/src/main.rs
+++ 20_4_12_abc162/d_rgb_triplets/src/main.rs
@@ -11,49 +11,20 @@ fn main() {
         buf.trim().chars().map(|c| c.to_string()).collect()
     };
 
-    let mut r_list: Vec<usize> = Vec::new();
-    let mut g_list: Vec<usize> = Vec::new();
-    let mut b_list: Vec<usize> = Vec::new();
-
-    for i in 0..n {
-        if s[i] == "R" {
-            r_list.push(i);
-        } else if s[i] == "G" {
-            g_list.push(i);
-        } else if s[i] == "B" {
-            b_list.push(i);
-        }
-    }
-
-    let r_len = r_list.len();
-    let g_len = g_list.len();
-    let b_len = g_list.len();
-
-    let mut g_start_index = 0;
-    let mut b_start_index = 0;
-
     let mut count = 0;
 
-    for i in 0..r_len {
-        for j in g_start_index..g_len {
-            if g_list[j] > r_list[i] {
-                let sub_j_i = g_list[j] - r_list[i];
-                for k in b_start_index..b_len {
-                    println!(
-                        "i: {}, j: {}, k: {}, r_list: {}, g_list: {}, b_list: {}",
-                        i, j, k, r_list[i], g_list[j], b_list[k]
-                    );
-                    if b_list[k] > g_list[j] {
-                        let sub_k_j = b_list[k] - g_list[j];
+    for i in 0..n {
+        for j in i + 1..n {
+            if s[i] != s[j] {
+                let sub_j_i = j - i;
+                for k in j + 1..n {
+                    if s[i] != s[k] && s[j] != s[k] {
+                        let sub_k_j = k - j;
                         if sub_k_j != sub_j_i {
                             count += 1;
                         }
-                    } else {
-                        b_start_index = k + 1;
                     }
                 }
-            } else {
-                g_start_index = j + 1;
             }
         }
     }
